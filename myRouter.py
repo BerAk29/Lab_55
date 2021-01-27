06e5c2dcc690aee03abd799901d772069dde0946
BerAk29 <bertrand.akoa@gmail.com>
2021-01-27 19:52:37 +0100

    Add class Switch then print

--- myRouter.py
+++ myRouter.py
@@ -14,21 +14,41 @@ class Router:
                 'Management IP': self.ip_add}
         return desc
 
+class Switch(Router):
+
+    def getdesc(self):
+        """ return a description from information """
+        desc = {'Router Model': self.model, 
+                'Software version': self.sw_version, 
+                 'Management IP': self.ip_add}
+        return desc
+
 rtr1 = Router('ASR1001-X', '16.12.4', '10.10.1.1')
 rtr2 = Router('ASR9901', '6.6.3', '10.10.2.2')
 rtr3 = Router('ISR4451', '16.6.5', '10.10.3.3')
+sw1 = Switch('Cat9500', '16.12.4', '10.10.10.10')
+
 
 desc1 = rtr1.getdesc()
 desc2 = rtr2.getdesc()
-print('\n')
+desc4 = sw1.getdesc()
+
+print(end='\n')
 for key, value in desc1.items():
     print(key, ':', value)
-print('\n')
+print(end='\n')
 for key, value in desc2.items():
     print(key, ':', value)
 
+print(end='\n')
+for key, value in desc4.items():
+    print(key, ':', value)
+
+"""
 #for item in desc1.items():
  #   print(item)
 
 import calendar as cal
 print('\n', cal.month(2021, 1, 2, 1))
+"""
+
